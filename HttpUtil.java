332b6b943cfd14c253e730df445bef54572c608b
topzy <648130682@qq.com>
2017-05-15 15:20:36 +0800

    Update

--- HttpUtil.java
+++ HttpUtil.java
@@ -1,4 +1,6 @@
+
 import com.alibaba.fastjson.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,23 +17,24 @@ import java.util.Map;
  */
 public class HttpUtil {
     /**
-     *  HTTP_POST 请求
+     * HTTP_POST 请求
+     *
      * @param requestUrl 请求地址
-     * @param jsonStr 请求参数JSON字符创
+     * @param jsonStr    请求参数JSON字符创
      * @return
      */
-    public static String  sendPostRequest(String requestUrl, String  jsonStr){
-        String result="";
-        if(isEmpty(requestUrl)){
+    public static String sendPostRequest(String requestUrl, String jsonStr) {
+        String result = "";
+        if (isEmpty(requestUrl)) {
             System.out.println("com.jy.HttpUtil.sendPostRequest方法中,参数requestUrl(请求地址)为空!");
             return "请求地址为空!";
         }
-        if(isEmpty(jsonStr)){
+        if (isEmpty(jsonStr)) {
             System.out.println("com.jy.HttpUtil.sendPostRequest方法中,参数jsonStr(请求参数)为空!");
             return "请求参数为空!";
         }
         try {
-            URL  url= new URL(requestUrl);
+            URL url = new URL(requestUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");//提交模式
             //是否允许输入输出
@@ -42,10 +45,10 @@ public class HttpUtil {
             //设置超时时间（毫秒）
             conn.setConnectTimeout(30000);
             //设置请求头信息
-            conn.setRequestProperty("Content-Type","application/json");
+            conn.setRequestProperty("Content-Type", "application/json");
             //连接地址
             conn.connect();
-            OutputStreamWriter  writer = new OutputStreamWriter(conn.getOutputStream());
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
             //发送参数
             writer.write(jsonStr);
             //清理当前编辑器的左右缓冲区，并使缓冲区数据写入基础流
@@ -53,18 +56,18 @@ public class HttpUtil {
             //关闭输出流
             writer.close();
             //读取返回结果
-            BufferedReader reader = new BufferedReader(new InputStreamReader( conn.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             //读取返回结果
-            String lines =reader.readLine();
+            String lines = reader.readLine();
             //关闭流
             reader.close();
-            JSONObject jsonObject=JSONObject.parseObject(lines);
+            JSONObject jsonObject = JSONObject.parseObject(lines);
             result = jsonObject.toString();
         } catch (MalformedURLException e) {
-            System.out.println("com.jy.HttpUtil,请求是失败"+e.getMessage());
+            System.out.println("com.jy.HttpUtil,请求是失败" + e.getMessage());
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("com.jy.HttpUtil.sendPostRequest,请求失败!"+e.getMessage());
+            System.out.println("com.jy.HttpUtil.sendPostRequest,请求失败!" + e.getMessage());
             e.printStackTrace();
         }
         return result;
@@ -72,65 +75,52 @@ public class HttpUtil {
 
 
     /**
-     *  请求参数分装
-     * @param params 请求参数 Map
+     * 请求参数分装
+     *
+     * @param  head 请求头信息
+     * @param  body 请求参数
      * @return
      */
-    public static  String getRequestParams(Map<String,Object> params){
-            JSONObject head = new JSONObject();
-            JSONObject body = new JSONObject();
-            head.put("username",params.get("username"));//账号
-            head.put("password", params.get("password"));//密码
-            //设置请求编码
-            head.put("requestCode", params.get("requestCode"));
+    public static String getRequestParams(Map<String, String> head, Map<String, String> body) {
+        JSONObject _head = new JSONObject();
+        JSONObject _body = new JSONObject();
+        _head.put("username", head.get("username"));//账号
+        _head.put("password", head.get("password"));//密码
 
-            head.put("dtype", "json");//目前只支持json格式
-            for (String key : params.keySet()) {
-                //添加参数时忽略username  password  requestCode
-                if("username".equals(key) || "password".equals(key) || "requestCode".equals(key)){
+        for (String key : body.keySet()) {
+           _body.put(key, body.get(key));
+        }
 
-                } else {
-                    body.put(key, params.get(key));
-                }
-            }
-            head.put("data", body);
-            return head.toString();
+        _head.put("data", _body);
+        return _head.toString();
     }
 
     /**
      * 获取数据入口
+     *
      * @param requestUrl 请求地址
-     * @param params  请求参数
+     * @param  head 请求头信息
+     * @param  body 请求参数
      * @return
      */
-    public static  String getData(String requestUrl,String requestCode,Map<String,Object> params){
-        requestUrl = requestUrl + "/" + requestCode;
-        params.put("requestCode",requestCode);
-        String  requestParams=getRequestParams(params);
-        String data="";
-        if(!isEmpty(requestParams)){
-        	System.out.println("请求URL---requestUrl:"+requestUrl);
-            System.out.println("请求参数---requestParams:"+requestParams);
-            data = sendPostRequest(requestUrl,requestParams);
+    public static String getData(String requestUrl, Map<String, String> head, Map<String, String> body) {
+        String requestParams = getRequestParams(head, body);
+        String data = "";
+        if (!isEmpty(requestParams)) {
+            System.out.println("请求URL---requestUrl:" + requestUrl);
+            System.out.println("请求参数---requestParams:" + requestParams);
+            data = sendPostRequest(requestUrl, requestParams);
         }
         return data;
     }
 
     /**
      * 判断字符串是否为空
-     * @param str
+     *
+     * @param s
      * @return
      */
-    public static boolean isNotEmpty(String str){
-        if(isEmpty(str)){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-
     public static boolean isEmpty(String s) {
-        return s == null || s.length() == 0 || s.trim().length()==0;
+        return s == null || s.length() == 0 || s.trim().length() == 0;
     }
 }
