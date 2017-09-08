968a15118aea3156ac6e35393d47e2e212aed183
topzy <648130682@qq.com>
2017-09-08 11:57:25 +0800

    Update HttpUtil.java

--- HttpUtil.java
+++ HttpUtil.java
@@ -73,7 +73,90 @@ public class HttpUtil {
         return result;
     }
 
+    /**
+     *  HTTP_POST 请求
+     * @param requestUrl 请求地址
+     * @param jsonStr 请求参数JSON字符创
+     * @return
+     */
+    public static JSONObject sendImagePostRequest(String requestUrl, String jsonStr, HttpServletResponse response) {
+        JSONObject jsonObject = null;
+        BufferedReader reader = null;
+        OutputStreamWriter writer = null;
+        HttpURLConnection conn = null;
+        InputStream inputStream = null;
+        OutputStream stream = null;
+        try {
+            URL url = new URL(requestUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");//提交模式
+            //是否允许输入输出
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            //是否允许缓存
+            conn.setUseCaches(false);
+            //设置超时时间（毫秒）
+            conn.setConnectTimeout(30000);
+            //设置请求头信息
+            conn.setRequestProperty("Content-Type", "application/json");
+            //连接地址
+            conn.connect();
+            writer = new OutputStreamWriter(conn.getOutputStream());
+            //发送参数
+            writer.write(jsonStr);
+            //清理当前编辑器的左右缓冲区，并使缓冲区数据写入基础流
+            writer.flush();
 
+            //读取图片流
+            stream = response.getOutputStream();
+            //读取返回结果
+            byte[] data = new byte[1024];
+            int len = 0;
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                inputStream = conn.getInputStream();
+                while ((len = inputStream.read(data)) != -1) {
+                    stream.write(data, 0, len);
+                }
+            }
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            //读取返回结果
+            String lines = reader.readLine();
+            jsonObject = JSONObject.parseObject(lines);
+        } catch (MalformedURLException e) {
+            logger.error("HttpUtil.sendPostRequest,请求失败!" + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.error("HttpUtil.sendPostRequest,请求失败!" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    //关闭流
+                    reader.close();
+                }
+                if (writer != null) {
+                    //关闭输出流
+                    writer.close();
+                }
+                if (conn != null) {
+                    //释放链接
+                    conn.disconnect();
+                }
+                if (inputStream != null) {
+                    //释放链接
+                    inputStream.close();
+                }
+                if (stream != null) {
+                    //释放链接
+                    stream.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonObject;
+    }
     /**
      * 请求参数分装
      *
@@ -113,7 +196,21 @@ public class HttpUtil {
         }
         return data;
     }
-
+    /**
+     * 获取图片入口
+     * @param requestUrl 请求地址
+     * @return
+     */
+    public static String getImg(String requestUrl, Map<String, String> head, Map<String, String> body, HttpServletResponse response) {
+        String requestParams = getRequestParams(head, body);
+        String data = "";
+        if (!isEmpty(requestParams)) {
+            System.out.println("请求URL---requestUrl:" + requestUrl);
+            System.out.println("请求参数---requestParams:" + requestParams);
+            data = sendImagePostRequest(requestUrl, requestParams, response).toJSONString();
+        }
+        return data;
+    }
     /**
      * 判断字符串是否为空
      *
